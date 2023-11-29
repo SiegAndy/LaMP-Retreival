7f7ddce0a8e4dcbe4b90bd86c59911f8efbc17f4
yk930608 <yk930608@gmail.com>
2023-11-29 15:33:07 -0500

    Added extract_token_status for prompt and output which includes total_tokens, avg_tokens(prompt only), price

--- src/util.py
+++ src/util.py
@@ -1,3 +1,4 @@
+import string
 from enum import Enum
 import json
 import os
@@ -5,6 +6,7 @@ import subprocess
 
 from json import JSONEncoder
 from typing import List
+from copy import copy
 
 
 class dto:
@@ -91,3 +93,33 @@ def copy2clip(txt):
         content = ", ".join(txt)
     cmd = "echo " + content + " | clip"
     return subprocess.check_call(cmd, shell=True)
+
+
+def number_of_tokens(prompt: str):
+    return len(prompt.split(" "))
+
+
+def remove_enter_punctuation(prompt: str):
+    new_prompt = copy(prompt)
+    for punctuation in string.punctuation:
+        new_prompt = new_prompt.replace(punctuation, "")
+    new_prompt.replace("\n"," ")
+    return new_prompt
+
+
+def extract_prompt_tokens_stats(flatten_prompts: List[str], price_per_1k_tokens: float = 0.001):
+    total_tokens = 0
+    for prompt in flatten_prompts:
+        cur_num_tokens = number_of_tokens(remove_enter_punctuation(prompt))
+        total_tokens += cur_num_tokens
+    return total_tokens, total_tokens / len(flatten_prompts), round(total_tokens * price_per_1k_tokens / 1000, 2)
+
+
+def extract_output_tokens_stats(output_json_path, price_per_1k_tokens: float = 0.002):
+    total_tokens = 0
+    with open(output_json_path, "r", encoding="utf-8") as file:
+        outputs = json.load(file)
+        for output in outputs["golds"]:
+            cur_num_tokens = number_of_tokens(output["output"])
+            total_tokens += cur_num_tokens
+    return total_tokens, round(total_tokens * price_per_1k_tokens / 1000, 2)
