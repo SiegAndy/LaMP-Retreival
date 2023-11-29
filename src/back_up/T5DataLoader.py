89e7b8b6292d9c4137c5b3b7504e1ac50a2dfc3c
yk930608 <yk930608@gmail.com>
2023-11-29 13:40:04 -0500

    Add move py files to back_up

---
+++ src/back_up/T5DataLoader.py
@@ -0,0 +1,48 @@
+from transformers import T5Tokenizer
+from src.back_up.T5Dataset import T5Dataset
+import pytorch_lightning as pl
+import torch
+
+
+class T5DataLoader(pl.LightningDataModule):
+
+    def __init__(self, train_data, valid_data, tokenizer: T5Tokenizer, input_max_len: int = 1024,
+                 output_max_len: int = 10):
+        super().__init__()
+        self.valid_dataset = None
+        self.train_dataset = None
+        self.train_data = train_data
+        self.valid_data = valid_data
+        self.tokenizer = tokenizer
+        self.input_max_len = input_max_len
+        self.out_max_len = output_max_len
+
+    def setup(self, stage=None):
+        self.train_dataset = T5Dataset(
+            question=self.train_data["questions"],
+            answer=self.train_data["labels"],
+            tokenizer=self.tokenizer
+        )
+
+        self.valid_dataset = T5Dataset(
+            question=self.valid_data["questions"],
+            answer=self.valid_data["labels"],
+            tokenizer=self.tokenizer
+        )
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=8,
+            shuffle=True,
+            num_workers=2,
+            persistent_workers=True
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.valid_dataset,
+            batch_size=2,
+            num_workers=2,
+            persistent_workers=True
+        )
---
+++ src/back_up/T5Model.py
@@ -0,0 +1,40 @@
+import pytorch_lightning as pl
+from torch.optim import AdamW
+
+
+class T5Model(pl.LightningModule):
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        output = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            labels=labels
+        )
+        return output.loss, output.logits
+
+    def training_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["target"]
+        loss, logits = self(input_ids, attention_mask, labels)
+
+        self.log("train_loss", loss, prog_bar=True, logger=True)
+
+        return {'loss': loss}
+
+    def validation_step(self, batch, batch_idx):
+        input_ids = batch["input_ids"]
+        attention_mask = batch["attention_mask"]
+        labels = batch["target"]
+        loss, logits = self(input_ids, attention_mask, labels)
+
+        self.log("val_loss", loss, prog_bar=True, logger=True)
+
+        return {'val_loss': loss}
+
+    def configure_optimizers(self):
+        return AdamW(self.parameters(), lr=0.0001)
